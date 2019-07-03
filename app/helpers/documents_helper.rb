27869f8cf2527179e15a78c83d7efc88a6b8f7a9
jwaldimer <jwaldimer@gmail.com>
2019-07-03 03:23:39 -0400

    added gem for Markdown processing and integrate with code

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -3,4 +3,18 @@ module DocumentsHelper
 	def document_old(date)
 		((Time.zone.now - date) / 1.day).to_i
 	end
+
+	def markdown(text)
+		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis: true,
+                                       fenced_code_blocks: true,
+                                       disable_indented_code_blocks: true,
+                                       autolink: true,
+                                       tables: true,
+                                       underline: true,
+                                       highlight: true
+                                      )
+    return markdown.render(text).html_safe
+  end
+
 end
